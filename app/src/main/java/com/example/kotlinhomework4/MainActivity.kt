1fba090ff0433d34ab2607bc930fd81fcf6315d9
Riley Ho <riley@rileychh.dev>
2024-11-29 18:46:32 +0800

    feat: open activities on button click

--- app/src/main/java/com/example/kotlinhomework4/MainActivity.kt
+++ app/src/main/java/com/example/kotlinhomework4/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.kotlinhomework4
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -19,6 +20,14 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.btnService.setOnClickListener {
+            startActivity(Intent(this, ServiceActivity::class.java))
+        }
+
+        binding.btnBroadcastReceiver.setOnClickListener {
+            startActivity(Intent(this, BroadcastReceiverActivity::class.java))
         }
     }
 }
