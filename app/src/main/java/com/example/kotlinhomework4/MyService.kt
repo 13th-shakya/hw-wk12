a2a7ecc270b2d025f251d5f2a1f794b80f23556e
Riley Ho <riley@rileychh.dev>
2024-11-29 19:06:27 +0800

    feat: add MyService

---
+++ app/src/main/java/com/example/kotlinhomework4/MyService.kt
@@ -0,0 +1,28 @@
+package com.example.kotlinhomework4
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class MyService : Service() {
+    override fun onCreate() {
+        super.onCreate()
+        Thread {
+            try {
+                Thread.sleep(3000)
+                val intent = Intent(this, ServiceStartActivity::class.java).apply {
+                    flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                }
+                startActivity(intent)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }.start()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return START_NOT_STICKY
+    }
+
+    override fun onBind(intent: Intent): IBinder? = null
+}
